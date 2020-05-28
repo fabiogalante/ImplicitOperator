9d03d2e1cbb63aaaeb94822e772ff40545491dbd
Fabio Galantes Mans <fmans.3mpk@sofisa.com.br>
2020-05-28 17:22:22 -0300

    Add project files.

---
+++ ImplicitOperator/Cliente.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImplicitOperator
+{
+    public class Cliente
+    {
+        public long Id { get; set; }
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public double Renda { get; set; }
+
+        public override string ToString()
+            => $"{Id} - {Nome} - {Sobrenome} - {DataNascimento} - {Renda}";
+
+
+        public static implicit operator Cliente(ClienteListViewModel professionResponse)
+        {
+            return new Cliente
+            {
+                Id = professionResponse.Id,
+                DataNascimento = professionResponse.DataNascimento,
+                Sobrenome = professionResponse.Sobrenome,
+                Nome = professionResponse.Nome,
+            };
+        }
+    }
+}
---
+++ ImplicitOperator/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ImplicitOperator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var clienteListViewModel = new ClienteListViewModel
+            {
+                DataNascimento = new DateTime(1982, 6, 12),
+                Id = 5,
+                Nome = "Jose",
+                Sobrenome = "Carlos"
+            };
+
+
+            var cliente = RetornarCliente(clienteListViewModel);
+
+            Console.WriteLine(cliente);
+
+
+        }
+
+
+        private static Cliente RetornarCliente(ClienteListViewModel cliente)
+        {
+            return cliente;
+        }
+    }
+}
